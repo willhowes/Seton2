f566a53964f6185333a4d492c3e470fe433a34ea
Will Howes <williameldenhowes@gmail.com>
2019-08-07 11:52:32 +0100

    Test results print to screen using document.write()

--- dave-1.0.0/dave.js
+++ dave-1.0.0/dave.js
@@ -1,5 +1,6 @@
 const describe = (desc, fn) => {
   console.log(desc)
+  document.write(desc)
   fn()
 }
 
@@ -16,12 +17,15 @@ const matchers = (exp) => ({
   toBe: (assertion) => {
     if (exp == assertion) {
       console.log('PASS - nice one geezer')
-
+      document.write('PASS - nice one geezer')
+      document.write("--------------")
       return true
     } else {
       console.log('FAIL - try again buddy')
+      document.write('FAIL - try again buddy')
       console.log(`got "${exp}" but expected "${assertion}"`)
-
+      document.write(`got "${exp}" but expected "${assertion}"`)
+      document.write("--------------")
       return false
     }
   }
