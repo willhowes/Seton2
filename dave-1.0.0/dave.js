c255370fd9421884596c9ce2ffb311dc7a9f15df
Will Howes <williameldenhowes@gmail.com>
2019-08-07 12:36:03 +0100

    (WIP) test result now in 'testarray'. Need to get it onto html page

--- dave-1.0.0/dave.js
+++ dave-1.0.0/dave.js
@@ -1,6 +1,10 @@
+var testarray = []
+
 const describe = (desc, fn) => {
+
   console.log(desc)
-  document.write(desc)
+  // document.write(desc)
+  testarray.push(desc)
   fn()
 }
 
@@ -17,16 +21,23 @@ const matchers = (exp) => ({
   toBe: (assertion) => {
     if (exp == assertion) {
       console.log('PASS - nice one geezer')
-      document.write('PASS - nice one geezer')
-      document.write("--------------")
+      testarray.push('PASS - nice one geezer')
+      // document.write('PASS - nice one geezer')
+      // document.write("--------------")
+      // document.getElementById("testPassFail").innerHTML = 'PASS - nice one geezer'
       return true
     } else {
       console.log('FAIL - try again buddy')
-      document.write('FAIL - try again buddy')
+      // document.write('FAIL - try again buddy')
       console.log(`got "${exp}" but expected "${assertion}"`)
-      document.write(`got "${exp}" but expected "${assertion}"`)
-      document.write("--------------")
+      // document.write(`got "${exp}" but expected "${assertion}"`)
+      // document.write("--------------")
+      testarray.push(`FAIL - try again buddy. Got "${exp}" but expected "${assertion}"`)
       return false
     }
   }
 })
+
+document.getElementById("testPassFail").innerHTML = testarray.join('')
+
+console.log(testarray.join(''))
