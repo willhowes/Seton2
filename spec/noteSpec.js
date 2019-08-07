c67f402219740987cc938e776d9f7f2599241adc
Charles Kettell <charleskettell@hotmail.co.uk>
2019-08-07 15:54:38 +0100

    added pass or fail to dave

--- spec/noteSpec.js
+++ spec/noteSpec.js
@@ -14,3 +14,11 @@ describe('#abbreviatedNote', () => {
     expect(note.abbreviatedNote()).toBe("This is my first not...")
   })
 })
+
+describe('#thisIsAFailingFunction', () => {
+  it('this will most certainly fail', () => {
+    note = new Note()
+    note.create("This is my first note which is definitely over 20 characters long")
+    expect(note.abbreviatedNote()).toBe("This is not mine...")
+  })
+})
